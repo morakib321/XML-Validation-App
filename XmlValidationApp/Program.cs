43d179977dd078ace2e1c4d683bbb5b8be88313c
Mohammed Rakib <124345592+morakib321@users.noreply.github.com>
2025-09-19 07:33:05 -0400

    Add files via upload

---
+++ XmlValidationApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews(); 
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapControllerRoute(
+    name: "xmlValidation",
+    pattern: "XmlValidation/{action=Index}/{id?}",
+    defaults: new { controller = "XmlValidation" });
+
+app.Run();
